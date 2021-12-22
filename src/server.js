66edf6dc18a990667845d07b3406756261293cfd
guillermo sandoval <s.guillermo85@gmail.com>
2021-12-22 17:06:09 -0300

    actualizaciones para heroku

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/ang-pokedex-app'));
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ang-pokedex-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
